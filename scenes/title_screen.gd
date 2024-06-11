cf9419f6393dbde29ed4e85ecbb12c0eb0245df3
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-06-11 06:52:17 -0400

    load darnell by default for now

--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 
 func _ready() -> void:
-	Chart.global = Chart.request("b4cksl4sh", "hard")
+	Chart.global = Chart.request("darnell", "hard")
 	await RenderingServer.frame_post_draw
 	get_tree().change_scene_to_packed(load("res://scenes/gameplay/gameplay.tscn"))
